7aa201c1ad03bb481cbe3a5885a1a7c2a0fe3ed1
up2u-admob <up2u.admob@gmail.com>
2014-11-08 17:46:34 +0900

    all contact

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+				
+  $("#page").on("swipeleft",function(){
+    $.mobile.changePage("#page2", {transition:"slide"}); 
+  });        
+    $("#page").on("swiperight",function(){
+    $.mobile.changePage("#page3", {transition:"slide", reverse: true}); 
+  });  
+  
+    $("#page2").on("swipeleft",function(){
+    $.mobile.changePage("#page3", {transition:"slide"}); 
+  });        
+    $("#page2").on("swiperight",function(){
+    $.mobile.changePage("#page", {transition:"slide", reverse: true}); 
+  });  
+  
+    $("#page3").on("swipeleft",function(){
+    $.mobile.changePage("#page", {transition:"slide"}); 
+  });        
+    $("#page3").on("swiperight",function(){
+    $.mobile.changePage("#page2", {transition:"slide", reverse: true}); 
+  });                
+
+});
