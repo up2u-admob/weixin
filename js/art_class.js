278bc60fc1cd53ce5561f71634d15cf1a08c8e85
up2u-admob <up2u.admob@gmail.com>
2014-11-13 18:12:55 +0900

    test46

--- js/art_class.js
+++ js/art_class.js
@@ -3,20 +3,3 @@ mainOutAni = "zoomOut";
 itemInAni = "zoomIn";
 itemOutAni = "zoomOut";
 
-$(document).ready(function () {
-  
-    $("#subpage1").on("swipeleft",function(){
-    $.mobile.changePage("#subpage2", {transition:"slide"}); 
-  });        
-    $("#subpage1").on("swiperight",function(){
-    $.mobile.changePage("#subpage2", {transition:"slide", reverse: true}); 
-  });  
-  
-    $("#subpage2").on("swipeleft",function(){
-    $.mobile.changePage("#subpage1", {transition:"slide"}); 
-  });        
-    $("#subpage2").on("swiperight",function(){
-    $.mobile.changePage("#subpage1", {transition:"slide", reverse: true}); 
-  });                
-
-});
