6e74647c830008fc9444f570ab082b086b8e5a34
up2u-admob <up2u.admob@gmail.com>
2014-11-13 18:21:37 +0900

    test49

--- js/art_class.js
+++ js/art_class.js
@@ -1,5 +1,10 @@
-//mainInAni = "zoomIn";
+mainInAni = "zoomIn";
 mainOutAni = "zoomOut";
 itemInAni = "zoomIn";
 itemOutAni = "zoomOut";
 
+$(document).ready(function () {   		
+	$("#m-mark").click(function(){
+		$.mobile.changePage("#subpage1", {transition:"flip"});
+	}); 
+});
