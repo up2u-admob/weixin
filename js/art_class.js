e1ae949b9cbe106e5b12c3b708330caf40cdb23c
up2u-admob <up2u.admob@gmail.com>
2014-11-13 18:27:07 +0900

    test51

--- js/art_class.js
+++ js/art_class.js
@@ -7,4 +7,22 @@ $(document).ready(function () {
 	$("#m-mark").click(function(){
 		$.mobile.changePage("#subpage1", {transition:"flip"});
 	}); 
+	$("#text1").click(function(){
+		$.mobile.changePage("#subpage1", {transition:"flip"});
+	}); 
+	$("#text2").click(function(){
+		$.mobile.changePage("#subpage1", {transition:"flip"});
+	}); 
+	$("#text3").click(function(){
+		$.mobile.changePage("#subpage1", {transition:"flip"});
+	}); 
+	$("#text4").click(function(){
+		$.mobile.changePage("#subpage1", {transition:"flip"});
+	}); 
+	$("#text5").click(function(){
+		$.mobile.changePage("#subpage1", {transition:"flip"});
+	}); 
+	$("#smalldot").click(function(){
+		$.mobile.changePage("#subpage1", {transition:"flip"});
+	}); 
 });
