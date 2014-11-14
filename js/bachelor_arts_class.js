f3dec114fe481df22abaf21514ad3a77baa3e092
up2u-admob <up2u.admob@gmail.com>
2014-11-14 13:01:00 +0900

    test

--- js/bachelor_arts_class.js
+++ js/bachelor_arts_class.js
@@ -1,4 +1,5 @@
 var sub_big = false;
+var delta = 0;
 
 var isTouch = ('ontouchstart' in window);
 
@@ -8,97 +9,56 @@ itemInAni = "flipInX";
 itemOutAni = "flipOutX";
 
 $(document).ready(function () {   
-		$("#m-mark img").bind({
-		    'touchstart mousedown': function(e) {
-		        e.preventDefault();
-		        this.pageX = (isTouch ? event.changedTouches[0].pageX : e.pageX);
-		        this.pageY = (isTouch ? event.changedTouches[0].pageY : e.pageY);
-		        this.left = parseInt($(this).css('left'));
-		        this.top = parseInt($(this).css('top'));
-		         
-		        this.touched = true;
-		    },
-		    'touchmove mousemove': function(e) {
-		        if (!this.touched) {
-		            return;
-		        }
-		        e.preventDefault();
-		        //alert($(this).css('left'));
-		        this.left = this.left - (this.pageX - (isTouch ? event.changedTouches[0].pageX : e.pageX) );
-		        this.top = this.top - (this.pageY - (isTouch ? event.changedTouches[0].pageY : e.pageY) );
-		        deltaX = this.pageX - (isTouch ? event.changedTouches[0].pageX : e.pageX);
-		        deltaY = this.pageY - (isTouch ? event.changedTouches[0].pageY : e.pageY);
-		        if (deltaY != 0)
-		        {
-		        	delta = deltaY/Math.abs(deltaY)*(deltaX*deltaX + deltaY*deltaY) * 10;
-			        console.log(deltaX + " " + deltaY);
-			        //$(this).css({'left':this.left, 'top':this.top});
-			        rotateMMark(delta + "deg");
-			    }
-		        this.pageX = (isTouch ? event.changedTouches[0].pageX : e.pageX);
-		        this.pageY = (isTouch ? event.changedTouches[0].pageY : e.pageY);
-		    },
-		    'touchend mouseup': function(e) {
-		        if (!this.touched) {
-		            return;
-		        }
-		        this.touched = false;
+	$("#m-mark img").bind({
+	    'touchstart mousedown': function(e) {
+	        e.preventDefault();
+	        this.pageX = (isTouch ? event.changedTouches[0].pageX : e.pageX);
+	        this.pageY = (isTouch ? event.changedTouches[0].pageY : e.pageY);
+	        this.left = parseInt($(this).css('left'));
+	        this.top = parseInt($(this).css('top'));
+	         
+	        this.touched = true;
+	        zoomMainClass("1", "-3%, -24%");
+	    },
+	    'touchmove mousemove': function(e) {
+	        if (!this.touched) {
+	            return;
+	        }
+	        e.preventDefault();
+	        //alert($(this).css('left'));
+	        this.left = this.left - (this.pageX - (isTouch ? event.changedTouches[0].pageX : e.pageX) );
+	        this.top = this.top - (this.pageY - (isTouch ? event.changedTouches[0].pageY : e.pageY) );
+	        deltaX = this.pageX - (isTouch ? event.changedTouches[0].pageX : e.pageX);
+	        deltaY = this.pageY - (isTouch ? event.changedTouches[0].pageY : e.pageY);
+	        if (Math.abs(deltaY) > 1)
+	        {
+	        	//console.log(deltaY);
+	        	delta = delta + deltaY/Math.abs(deltaY)*(deltaX*deltaX + deltaY*deltaY) / 5;
+		        rotateMMark(delta);
 		    }
-		});
+	        this.pageX = (isTouch ? event.changedTouches[0].pageX : e.pageX);
+	        this.pageY = (isTouch ? event.changedTouches[0].pageY : e.pageY);
+	    },
+	    'touchend mouseup': function(e) {
+	        if (!this.touched) {
+	            return;
+	        }
+	        this.touched = false;
+	    }
+	});
 
-	$("#sub1").click(changeSubBlock1); 
-	$("#sub2").click(changeSubBlock2); 
-	$("#sub3").click(changeSubBlock3); 
-	$("#sub4").click(changeSubBlock4); 
+	$("#sub1").click(function(){changeSubBlock(-40);}); 
+	$("#sub2").click(function(){changeSubBlock(-130);}); 
+	$("#sub3").click(function(){changeSubBlock(-220);}); 
+	$("#sub4").click(function(){changeSubBlock(-310);}); 
 });
 
-function changeSubBlock1()
+function changeSubBlock(degree)
 {
 	if (!sub_big)
 	{
-		rotateMMark("-40deg");
-		zoomMainClass("1", "-3%, -24%");
-	 	sub_big = true;
-	}
-	else
-	{
-		restore();
-	}
-}
-
-function changeSubBlock2()
-{
-	if (!sub_big)
-	{
-		rotateMMark("-130deg");
-		zoomMainClass("1", "-3%, -24%");
-	 	sub_big = true;
-	}
-	else
-	{
-		restore();
-	}
-}
-
-function changeSubBlock3()
-{
-	if (!sub_big)
-	{
-		rotateMMark("-210deg");
-		zoomMainClass("1", "-3%, -24%");
-	 	sub_big = true;
-	}
-	else
-	{
-		restore();
-	}
-}
-
-function changeSubBlock4()
-{
-	if (!sub_big)
-	{
-		rotateMMark("-300deg");
+		delta = degree;
+		rotateMMark(delta+"deg");
 		zoomMainClass("1", "-3%, -24%");
 	 	sub_big = true;
 	}
@@ -110,7 +70,7 @@ function changeSubBlock4()
 
 function restore()
 {
-	rotateMMark("0deg");
+	rotateMMark(0);
 	zoomMainClass("0.4", "0%, 0%");
 	sub_big = false;
 }
@@ -118,8 +78,8 @@ function restore()
 function rotateMMark(rotateParameter)
 {
 	$("#m-mark img").css({ 
-		"-webkit-transform":"rotate("+rotateParameter+")",
-		"-moz-transform":"rotate("+rotateParameter+"",
+		"-webkit-transform":"rotate("+rotateParameter+"deg)",
+		"-moz-transform":"rotate("+rotateParameter+"deg)",
     	"-webkit-transform-origin": "center center", 
 		"-moz-transform-origin": "center center",  
 		"-webkit-transition":"-webkit-transform 1s linear 0s",
