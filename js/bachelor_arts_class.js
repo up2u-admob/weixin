af30a44ed7ed95c42d4ed14c5495c2e25424469a
up2u-admob <up2u.admob@gmail.com>
2014-11-14 11:41:08 +0900

    test rotation

--- js/bachelor_arts_class.js
+++ js/bachelor_arts_class.js
@@ -1,11 +1,51 @@
 var sub_big = false;
 
+var isTouch = ('ontouchstart' in window);
+
 mainInAni = "rollIn";
 mainOutAni = "rollOut";
 itemInAni = "flipInX";
 itemOutAni = "flipOutX";
 
 $(document).ready(function () {   
+		$("#m-mark img").bind({
+		    'touchstart mousedown': function(e) {
+		        e.preventDefault();
+		        this.pageX = (isTouch ? event.changedTouches[0].pageX : e.pageX);
+		        this.pageY = (isTouch ? event.changedTouches[0].pageY : e.pageY);
+		        this.left = parseInt($(this).css('left'));
+		        this.top = parseInt($(this).css('top'));
+		         
+		        this.touched = true;
+		    },
+		    'touchmove mousemove': function(e) {
+		        if (!this.touched) {
+		            return;
+		        }
+		        e.preventDefault();
+		        //alert($(this).css('left'));
+		        this.left = this.left - (this.pageX - (isTouch ? event.changedTouches[0].pageX : e.pageX) );
+		        this.top = this.top - (this.pageY - (isTouch ? event.changedTouches[0].pageY : e.pageY) );
+		        deltaX = this.pageX - (isTouch ? event.changedTouches[0].pageX : e.pageX);
+		        deltaY = this.pageY - (isTouch ? event.changedTouches[0].pageY : e.pageY);
+		        if (deltaY != 0)
+		        {
+		        	delta = deltaY/Math.abs(deltaY)*(deltaX*deltaX + deltaY*deltaY) * 10;
+			        console.log(deltaX + " " + deltaY);
+			        //$(this).css({'left':this.left, 'top':this.top});
+			        rotateMMark(delta + "deg");
+			    }
+		        this.pageX = (isTouch ? event.changedTouches[0].pageX : e.pageX);
+		        this.pageY = (isTouch ? event.changedTouches[0].pageY : e.pageY);
+		    },
+		    'touchend mouseup': function(e) {
+		        if (!this.touched) {
+		            return;
+		        }
+		        this.touched = false;
+		    }
+		});
+
 	$("#sub1").click(changeSubBlock1); 
 	$("#sub2").click(changeSubBlock2); 
 	$("#sub3").click(changeSubBlock3); 
